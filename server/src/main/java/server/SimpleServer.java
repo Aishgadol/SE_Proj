a5f067d9ea4c8ece79ab62f56d7c25df779ff273
unknown <xanjera@gmail.com>
2024-07-09 16:47:50 +0300

    rer=irder functions in server

--- server/src/main/java/server/SimpleServer.java
+++ server/src/main/java/server/SimpleServer.java
@@ -101,6 +101,27 @@ public class SimpleServer extends AbstractServer {
 		return null;
 	}
 
+	private List<Msg> getMsgs(){
+		CriteriaBuilder builder=session.getCriteriaBuilder();
+		CriteriaQuery<Msg> query=builder.createQuery(Msg.class);
+		query.from(Msg.class);
+		List<Msg> msgs=session.createQuery(query).getResultList();
+		return msgs;
+	}
+
+	private DisplayTime getDisplayTimeFromDB(String displayTimeString){
+		CriteriaBuilder builder=session.getCriteriaBuilder();
+		CriteriaQuery<DisplayTime> query=builder.createQuery(DisplayTime.class);
+		Root<DisplayTime> root=query.from(DisplayTime.class);
+		Predicate titlePredicate=builder.equal(root.get("Display_Time_And_Date"),displayTimeString);
+		query.where(titlePredicate);
+		List<DisplayTime> dis=session.createQuery(query).getResultList();
+		if(dis!=null && !dis.isEmpty()){
+			return dis.get(0);
+		}
+		return null;
+	}
+
 	// updates display time for the current movie selected
 	public void addDisplayTimeToDB(String time){
 		this.currMovie=getMovieByTitleFromDB(this.currMovie.getName());
@@ -142,6 +163,14 @@ public class SimpleServer extends AbstractServer {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+
+	public static void addMsgToDB(String text) throws Exception{
+		Msg m=new Msg(text);
+		session.save(m);
+		session.flush();
+		session.getTransaction().commit();
+		session.beginTransaction();
 	}
 
 	void removeDisplayTimeFromMovieFromDB(String displayTime) {
@@ -193,35 +222,6 @@ public class SimpleServer extends AbstractServer {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void addMsgToDB(String text) throws Exception{
-		Msg m=new Msg(text);
-		session.save(m);
-		session.flush();
-		session.getTransaction().commit();
-		session.beginTransaction();
-	}
-
-	private List<Msg> getMsgs(){
-		CriteriaBuilder builder=session.getCriteriaBuilder();
-		CriteriaQuery<Msg> query=builder.createQuery(Msg.class);
-		query.from(Msg.class);
-		List<Msg> msgs=session.createQuery(query).getResultList();
-		return msgs;
-	}
-
-	private DisplayTime getDisplayTimeFromDB(String displayTimeString){
-		CriteriaBuilder builder=session.getCriteriaBuilder();
-		CriteriaQuery<DisplayTime> query=builder.createQuery(DisplayTime.class);
-		Root<DisplayTime> root=query.from(DisplayTime.class);
-		Predicate titlePredicate=builder.equal(root.get("Display_Time_And_Date"),displayTimeString);
-		query.where(titlePredicate);
-		List<DisplayTime> dis=session.createQuery(query).getResultList();
-		if(dis!=null && !dis.isEmpty()){
-			return dis.get(0);
-		}
-		return null;
 	}
 
 	private void setMovieInfos(){
@@ -298,72 +298,7 @@ public class SimpleServer extends AbstractServer {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		/*
-			//we got an empty message, so we will send back an error message with the error details.
-			if (request.isBlank()){
-				message.setMessage("Error! we got an empty message");
-				client.sendToClient(message);
-			}
-			//we got a request to change submitters IDs with the updated IDs at the end of the string, so we save
-			// the IDs at data field in Message entity and send back to all subscribed clients a request to update
-			//their IDs text fields. An example of use of observer design pattern.
-			//message format: "change submitters IDs: 123456789, 987654321"
-			else if(request.startsWith("change submitters IDs:")){
-				message.setData(request.substring(23));
-				message.setMessage("update submitters IDs");
-				sendToAllClients(message);
-			}
-			//we got a request to add a new client as a subscriber.
-			else if (request.startsWith("add client")){
-				SubscribedClient connection = new SubscribedClient(client);
-				SubscribersList.add(connection);
-				message.setMessage("client added successfully");
-				client.sendToClient(message);
-			}
-			//we got a message from client requesting to echo Hello, so we will send back to client Hello world!
-			else if(request.startsWith("echo Hello")){
-				message.setMessage("Hello World!");
-				client.sendToClient(message);
-			}
-			else if(request.startsWith("send Submitters IDs")){
-				//add code here to send submitters IDs to client
-				message.setMessage("316451012, 206234874");
-				client.sendToClient(message);
-
-			}
-			else if (request.startsWith("send Submitters")){
-				//add code here to send submitters names to client
-				message.setMessage("Idan, Nir");
-				client.sendToClient(message);
-			}
-			else if (request.contains("time")) {
-				//add code here to send the time to client
-				message.setMessage(message.getTimeStamp().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-				//client.sendToClient(message);
-				client.sendToClient(message);
-			}
-			else if (request.startsWith("multiply")){
-				//add code here to multiply 2 numbers received in the message and send result back to client
-				//(use substring method as shown above)
-				//message format: "multiply n*m"
-				int n=Integer.parseInt(request.substring(9,10));
-				int m=Integer.parseInt(request.substring(11,12));
-				message.setMessage(n+" * "+m+" = "+(n*m));
-				client.sendToClient(message);
-			}else{
-				//add code here to send received message to all clients.
-				//The string we received in the message is the message we will send back to all clients subscribed.
-				//Example:
-					// message received: "Good morning"
-					// message sent: "Good morning"
-				//see code for changing submitters IDs for help
-				sendToAllClients(message);
-			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}*/
 	}
-
 	 	/*
 	 	THIS PART WILL BE USEFUL TO ADDRESS AND UPDATE ALL CLIENTS, DATABASE STUFF FOR EXAMPLE
 	 	 */
